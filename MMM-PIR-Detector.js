bd4ce39e3c013aa32248f33fc8d6d0d86b6d1e09
Paul Shuster <shu3636@hotmail.com>
2017-01-19 23:04:26 -0500

    simplifying

--- MMM-PIR-Detector.js
+++ MMM-PIR-Detector.js
@@ -10,41 +10,41 @@
 
 Module.register('MMM-PIR-Detector',{
 
-	defaults: {
-		pirPIN: 22,
-		powerSaving: true,
-		relayOnState: 1,
-	},
+  defaults: {
+    pirPIN: 22,
+    powerSaving: true,
+    relayOnState: 1,
+  },
  
+/*
+  // Override socket notification handler.
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "USER_PRESENCE"){
+      this.sendNotification(notification, payload)
+    }
+  },
 
-	// Override socket notification handler.
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "USER_PRESENCE"){
-			this.sendNotification(notification, payload)
-		}
-	},
-
-	notificationReceived: function(notification, payload) {
-		if (notification === "SCREEN_WAKEUP"){
-			this.sendNotification(notification, payload)
-		}
-	},
-
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = "Testing this out";
-		return wrapper;
-	}
-
-	start: function() {
-		if (this.config.relayOnState == 1){
-			this.config.relayOffState = 0
-		}
-		else if (this.config.relayOnState == 0){
-			this.config.relayOffState = 1
-		}
-		this.sendSocketNotification('CONFIG', this.config);
-		Log.info('Starting module: ' + this.name);
+  notificationReceived: function(notification, payload) {
+    if (notification === "SCREEN_WAKEUP"){
+      this.sendNotification(notification, payload)
+    }
+  },
+*/
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = "Testing this out";
+    return wrapper;
+  }
+/*
+  start: function() {
+    if (this.config.relayOnState == 1){
+      this.config.relayOffState = 0
+    }
+    else if (this.config.relayOnState == 0){
+      this.config.relayOffState = 1
+    }
+    this.sendSocketNotification('CONFIG', this.config);
+    Log.info('Starting module: ' + this.name);
     this.updateDom(1000);
-	}
+  }*/
 });
